975e6a826cba5a2cb4ec660be2e809173c584b5a
jackjnn <jacknyangenj.jn2@gmail.com>
2021-05-24 16:10:22 +0300

    App can now play audio/mp3

--- player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
+++ player-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -15,13 +15,16 @@
  */
 package com.example.exoplayer;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.util.Util;
 
 
 /**
@@ -31,6 +34,9 @@ public class PlayerActivity extends AppCompatActivity {
 
   private PlayerView playerView;
   private SimpleExoPlayer player;
+  private boolean playWhenReady = true;
+  private int currentWindow = 0;
+  private long playbackPosition = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +45,67 @@ public class PlayerActivity extends AppCompatActivity {
     playerView = findViewById(R.id.video_view);
   }
 
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if (Util.SDK_INT >= 24) {
+      initializePlayer();
+    }
+  }
 
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (Util.SDK_INT >= 24) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (Util.SDK_INT < 24) {
+      releasePlayer();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    hideSystemUI();
+    if ((Util.SDK_INT < 24 || player == null)) {
+      initializePlayer();
+    }
+  }
+
+  @SuppressLint("InlinedApi")
+  private void hideSystemUI() {
+    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+  }
 
   private void initializePlayer(){
     player = new SimpleExoPlayer.Builder(this).build();
     playerView.setPlayer(player);
     MediaItem mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3));
     player.setMediaItem(mediaItem);
+    player.setPlayWhenReady(playWhenReady);
+    player.seekTo(currentWindow, playbackPosition);
+    player.prepare();
+  }
+
+  private void releasePlayer() {
+    if (player != null) {
+      playWhenReady = player.getPlayWhenReady();
+      playbackPosition = player.getCurrentPosition();
+      currentWindow = player.getCurrentWindowIndex();
+      player.release();
+      player = null;
+    }
   }
 
 }
